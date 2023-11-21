0acb6cdaa1c6c2378b738dcf3050af75ed37313c
L1oid <71085120+L1oid@users.noreply.github.com>
2023-11-22 02:03:12 +0700

    Delete backend/auth directory

--- backend/auth/src/main/java/pack/application/auth/service/api/Sendable.java
+++
@@ -1,8 +0,0 @@
-package pack.application.auth.service.api;
-
-public interface Sendable {
-    void sendCheckUser(Boolean status);
-    void sendAddUser(Boolean status);
-    void sendCheckToken(Boolean status);
-    void sendCreateToken(String token);
-}
